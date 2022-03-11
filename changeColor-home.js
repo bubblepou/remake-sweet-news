a10a22c3e809bf2f9a5b72f544b7b732fe1c3050
bubblepou <86073706+bubblepou@users.noreply.github.com>
2022-03-11 22:01:17 +0800

    首頁圖片 bug 修復

--- changeColor-home.js
+++ changeColor-home.js
@@ -1,36 +1,65 @@
-if (localStorage.getItem('color') == 'strawberry'){
+if (localStorage.getItem('color') == 'strawberry') {
     fStrawberry()
 }
-else if(localStorage.getItem('color') == 'choco'){
+else if (localStorage.getItem('color') == 'choco') {
     fChoco()
 }
-else if(localStorage.getItem('color') == 'mont'){
+else if (localStorage.getItem('color') == 'mont') {
     fMont()
 }
 
-function fStrawberry(){
-document.getElementById('web-style').href="./strawberry-home.css";
-document.getElementById('cuteimg').src="./man/strawberry-cute.png";
-localStorage.setItem('color','strawberry');
+function fStrawberry() {
+    document.getElementById('web-style').href = "./strawberry-home.css";
+    document.getElementById('cuteimg').src = "./man/strawberry-cute.png";
+    localStorage.setItem('color', 'strawberry');
 }
 
-function fChoco(){
-    document.getElementById('web-style').href="./choco-home.css";
-    document.getElementById('cuteimg').src="./man/choco-cute.png";
-    localStorage.setItem('color','choco');
+function fChoco() {
+    document.getElementById('web-style').href = "./choco-home.css";
+    document.getElementById('cuteimg').src = "./man/choco-cute.png";
+    localStorage.setItem('color', 'choco');
 }
 
-function fMont(){
-    document.getElementById('web-style').href="./mont-home.css";
-    document.getElementById('cuteimg').src="./man/mont-cute.png";
-    localStorage.setItem('color','mont');
+function fMont() {
+    document.getElementById('web-style').href = "./mont-home.css";
+    document.getElementById('cuteimg').src = "./man/mont-cute.png";
+    localStorage.setItem('color', 'mont');
 }
 
-let sBtn =  document.getElementById('strawberry');
-sBtn.addEventListener('click',fStrawberry)
+let sBtn = document.getElementById('strawberry');
+sBtn.addEventListener('click', fStrawberry)
 
-let cBtn =  document.getElementById('choco');
-cBtn.addEventListener('click',fChoco)
+let cBtn = document.getElementById('choco');
+cBtn.addEventListener('click', fChoco)
 
-let mBtn =  document.getElementById('mont');
-mBtn.addEventListener('click',fMont)
+let mBtn = document.getElementById('mont');
+mBtn.addEventListener('click', fMont)
+
+
+
+/*resize image*/
+
+let images = document.getElementsByClassName('key-new-image')
+let div = document.getElementsByClassName('key-new')
+
+Change_Height();
+
+
+function Change_Height() {
+    for (let i = 0; i < images.length; i++) {
+        let div_height = div[i].clientHeight;
+        let image_height = images[i].clientHeight;
+        let div_weight = div[i].clientWidth;
+        let image_weight = images[i].clientWidth;
+        if (image_height - div_height < 0 && image_weight - div_weight >= 0) {
+            images[i].style.height = "100%";
+            images[i].style.weight = "auto";
+        }
+        else if (image_height - div_height >= 0 && image_weight - div_weight < 0) {
+            images[i].style.height = "auto";
+            images[i].style.weight = "100%";
+        };
+    };
+}
+
+window.onresize = Change_Height;
